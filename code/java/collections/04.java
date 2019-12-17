28dbc00ab24aac69d43f68edf1fe3688b2cd0e4b
Willi Schönborn <w.schoenborn@gmail.com>
2019-12-17 08:53:31 +0100

    Remove unnecessary variables

--- code/java/collections/04.java
+++ code/java/collections/04.java
@@ -1,7 +1,2 @@
-final List&ltInteger&gt evens = new ArrayList<>();
-final List&ltInteger&gt odds = new ArrayList<>();
-
 Map&ltBoolean, List&ltInteger&gt&gt map = numbers.stream()
-        .collect(
-            Collectors.partitioningBy(num -> num % 2 == 0)
-        );
+        .collect(partitioningBy(num -> num % 2 == 0));
