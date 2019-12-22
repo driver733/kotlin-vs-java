f68483e404b8d51c6cb3060fef9a2a93068eb085
Simon Struck <simon@simonscode.org>
2019-12-22 11:59:15 +0100

    Fix lambda indentation

--- code/java/concurrency/02.java
+++ code/java/concurrency/02.java
@@ -1,13 +1,13 @@
 public <A> CompletableFuture<A> inTransaction(DatabaseFunction<A> fun) {
     return CompletableFuture
     .runAsync(() -> this.sendQuery("BEGIN"))
-    .thenApply(x -> fun.execute(this))
+    .thenApply(ignored -> fun.execute(this))
     .thenApply(s -> {
-    this.sendQuery("COMMIT");
-    return s;
+        this.sendQuery("COMMIT");
+        return s;
     })
-    .exceptionally(throwable -> {
-    this.sendQuery("ROLLBACK");
-    return null;
+        .exceptionally(throwable -> {
+        this.sendQuery("ROLLBACK");
+        return null;
     });
 }
