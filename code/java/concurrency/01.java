6c5b2dd01d280f9a13be83b1f1c811de9190e6af
Mikhail Yakushin <driver733@gmail.com>
2020-02-27 16:51:36 +0300

    #95 - completed

--- code/java/concurrency/01.java
+++ code/java/concurrency/01.java
@@ -1,7 +1,6 @@
 public CompletableFuture&ltSomeOtherResult&gt loadAndCombine(String name1, String name2){
         loadImageAsync(name1)
-            .thenAcceptBoth(loadImageAsync(name2)) (
-                (one, two) -> {
-                // combine results of both calls here
+            .thenAcceptBoth(loadImageAsync(name2)) ((one, two) -> {
+                combineImages(deferred1.await(), deferred2.await())
         )}
 }
